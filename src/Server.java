65d91ceb8b5b54ad924d4e346594627d469ef4db
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-02-15 10:37:07 +0000

    codigo prof

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,28 @@
-public class Server{
-    
+import java.net.*;
+import java.io.IOException;
+
+public class Server extends Throwable{
+    public static void main(String[] args) throws IOException{
+        if (args.length != 2) {
+            System.out.println("Usage: java Echo <hostname> <string to echo>");
+            return;
+        }
+
+        // send request
+        DatagramSocket socket = new DatagramSocket();
+        byte[] sbuf = args[1].getBytes();
+        InetAddress address = InetAddress.getByName(args[0]);
+        DatagramPacket packet = new DatagramPacket(sbuf, sbuf.length,address, 4445);
+        socket.send(packet);
+
+        // get response
+        byte[] rbuf = new byte[sbuf.length];
+        packet = new DatagramPacket(rbuf, rbuf.length);
+        socket.receive(packet);
+        
+        // display response
+        String received = new String(packet.getData());
+        System.out.println("Echoed Message: " + received);
+        socket.close();
+    }
 }
