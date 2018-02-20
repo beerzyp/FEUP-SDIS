3a2860ba491a024e892694b7396b9233c72f4b73
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-02-20 17:26:32 +0000

    client

---
+++ src/Client.java
@@ -0,0 +1,9 @@
+import java.net.*;
+import java.io.IOException;
+import java.lang.System.*;
+
+public class Client extends Throwable {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
