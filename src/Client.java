e083fe447f93bd00b29892a9bde66e553d20b29b
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-02-20 19:30:35 +0000

    70% ✅

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,58 @@
 import java.net.*;
+import java.io.*;
 import java.io.IOException;
 import java.lang.System.*;
+import java.lang.Integer;
 
+import Server;
+
+//
 public class Client extends Throwable {
-    public static void main(String[] args) throws IOException {
 
+    public static int port = 4445; // server port
+
+    public static void main(String[] args) throws IOException {
+        if(args.length != 4){
+            System.err.println("Usage: java Client <host_name> <port_number> <oper> <opnd>");
+            return;
+        }
+
+        /*
+        int port;
+        InetAddress address;
+        DatagramSocket socket = null;
+        DatagramPacket packet;
+        byte[] sendBuf = new byte[256];
+        */
+
+        /*
+        String hostname = args[0];
+        System.out.println(hostname+"\n");
+
+        int port = Integer.parseInt(args[1]);
+        System.out.println(port+"\n");
+        */
+
+        DatagramSocket socket = new DatagramSocket();
+
+        sendRequest(socket, "abs");
+    }
+
+    // send request
+    public static void sendRequest(DatagramSocket socket, String ans) throws IOException{
+        InetAddress address = InetAddress.getLocalHost();
+        //correto
+        //byte[] sbuf = ans.getBytes();
+        //try
+        byte[] rbuf = {'1','2'};
+        DatagramPacket packet = new DatagramPacket(rbuf, rbuf.length,address, Server.port); //port
+        socket.send(packet);
+    }
+
+    // display response
+    public static void displayResponse(DatagramPacket packet, DatagramSocket socket){
+        String received = new String(packet.getData());
+        System.out.println("Echoed Message: " + received);
+        socket.close();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,38 +1,63 @@
 import java.net.*;
+import java.io.*;
 import java.io.IOException;
 import java.lang.System.*;
 
+/*
+Compilar: javac Server.java
+Correr: java Filename
+ */
+
 public class Server extends Throwable{
+
+    public static int port = 4445; // server port
+
     public static void main(String[] args) throws IOException{
         if (args.length != 2) {
             System.out.println("Usage: java Echo <hostname> <string to echo>");
             return;
         }
 
-        // send request
-        DatagramSocket socket = new DatagramSocket();
-        byte[] sbuf = args[1].getBytes();
-        InetAddress address = InetAddress.getByName(args[0]);
-        DatagramPacket packet = new DatagramPacket(sbuf, sbuf.length,address, 4445);
-        socket.send(packet);
+        DatagramSocket socket = new DatagramSocket(port);//4445
 
-        // get response
-        byte[] rbuf = new byte[sbuf.length];
-        packet = new DatagramPacket(rbuf, rbuf.length);
-        socket.receive(packet);
-        
-        // display response
+        getResponse(socket);
+    }
+
+    // send request
+    public static void sendRequest(DatagramSocket socket, String ans) throws IOException{
+        System.out.println("xbxbxbxb 2");
+        byte[] sbuf = ans.getBytes();
+        InetAddress address = InetAddress.getLocalHost();
+        DatagramPacket packet = new DatagramPacket(sbuf, sbuf.length,address, port); //4445
+        socket.send(packet);
+    }
+
+    // display response
+    public static void displayResponse(DatagramPacket packet, DatagramSocket socket){
         String received = new String(packet.getData());
         System.out.println("Echoed Message: " + received);
         socket.close();
     }
+    // get response
+    public static void getResponse(DatagramSocket socket) throws IOException{
+        InetAddress address = InetAddress.getLocalHost();
 
-    public boolean validatePort(String args[]){
-        if(args.length != 2){
-            System.err.println("java Server <port_number>");
-            return false;
-        }else{
-            return true;
+        while (true) {
+            byte[] rbuf = new byte[256];
+            //byte[] rbuf = {'1','2'};
+            DatagramPacket packet = new DatagramPacket(rbuf, rbuf.length);
+
+            try {
+                System.out.println("xbxbxbxb");
+                socket.receive(packet);
+                String ans = new String(packet.getData());
+                System.out.println("antes send request");
+                sendRequest(socket,ans);
+            }catch (IOException e){
+                System.err.println("Socket error");
+                throw new IOException("Socket error");
+            }
+
         }
     }
 }
