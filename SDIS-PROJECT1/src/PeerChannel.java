3cde92a89cc50ebb9c4c28bf8fcd9dcb2b704269
beerzyp <up201505092@fe.up.pt>
2018-03-23 19:38:43 +0000

    p2p connection and hard coded message working
    TODO:
    -make main w/  chunk backup chunk restore file deletion space reclaiming CASES:
    -Start with the BACKUP case

--- SDIS-PROJECT1/src/PeerChannel.java
+++ SDIS-PROJECT1/src/PeerChannel.java
@@ -11,7 +11,7 @@ public class PeerChannel {
     private String peerName;
 
     public PeerChannel(InetAddress mcAddr, int mcastPort,Peer newPeer) throws IOException{
-    this.mcAddr=mcAddr;
+    this.mcAddr=InetAddress.getByName("228.5.6.7"); //localhost is not in range in multicasting
     this.mcPort=mcastPort;
     peerName="Peer"+Integer.toString(newPeer.peerID);
     newPeer.incrementPeerId();
@@ -23,7 +23,7 @@ public class PeerChannel {
             mcSocket.joinGroup(this.mcAddr);
 
         }
-        catch (SocketException e){System.out.println("socket creation error");}
+        catch (SocketException e){System.out.println("socket creation error");e.printStackTrace();}
 
 
     }
@@ -43,16 +43,18 @@ public class PeerChannel {
         s_buffer[2]='A';
         DatagramPacket sendPacket = new DatagramPacket(s_buffer, s_buffer.length, this.mcAddr, this.mcPort);
         this.mcSocket.send(sendPacket);
+        System.out.println("\nmessage sent");
         return 0;
     }
 
     int recieveMessage() throws IOException{
         byte[] r_buffer = new byte[1024];
         DatagramPacket receivePacket = new DatagramPacket(r_buffer, r_buffer.length);
-        this.mcSocket.receive(receivePacket);
+
         System.out.println(r_buffer[0]);
         System.out.println(r_buffer[1]);
         System.out.println(r_buffer[2]);
+        System.out.println("\n message recieved");
         return 0;
     }
 }
